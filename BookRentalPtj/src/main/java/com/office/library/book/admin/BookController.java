2dafbf08adb3ff9493b97c3cde0a049d1f0375ef
shimsGram <shims5980@daum.net>
2024-12-02 17:25:10 +0900

    14장 완성

--- BookRentalPtj/src/main/java/com/office/library/book/admin/BookController.java
+++ BookRentalPtj/src/main/java/com/office/library/book/admin/BookController.java
@@ -10,11 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.office.library.admin.member.AdminMemberVo;
 import com.office.library.book.BookVo;
+import com.office.library.book.HopeBookVo;
 import com.office.library.book.RentalBookVo;
 import com.office.library.book.admin.util.UploadFileService;
 
@@ -224,6 +226,91 @@ public class BookController {
 		
 		if (result <= 0)
 			nextPage = "admin/book/return_book_ng";
+		
+		return nextPage;
+		
+	}
+	
+	/*
+	 * 희망 도서 목록
+	 */
+//	@RequestMapping(value = "/getHopeBooks", method = RequestMethod.GET)
+	@GetMapping("/getHopeBooks")
+	public String getHopeBooks(Model model) {
+		System.out.println("[BookController] getHopeBooks()");
+		
+		String nextPage = "admin/book/hope_books";
+		
+		List<HopeBookVo> hopeBookVos = bookService.getHopeBooks();
+		
+		model.addAttribute("hopeBookVos", hopeBookVos);
+		
+		return nextPage;
+		
+	}
+	
+	/*
+	 * 희망 도서 등록(입고 처리)
+	 */
+//	@RequestMapping(value = "/registerHopeBookForm", method = RequestMethod.GET)
+	@GetMapping("/registerHopeBookForm")
+	public String registerHopeBookForm(Model model, HopeBookVo hopeBookVo) {
+		System.out.println("[BookController] registerHopeBookForm()");
+		
+		String nextPage = "admin/book/register_hope_book_form";
+		
+		model.addAttribute("hopeBookVo", hopeBookVo);
+		
+		return nextPage;
+		
+	}
+	
+	/*
+	 * 희망 도서 등록(입고 처리) 확인
+	 */
+//	@RequestMapping(value = "/registerHopeBookConfirm", method = RequestMethod.POST)
+	@PostMapping("/registerHopeBookConfirm")
+	public String registerHopeBookConfirm(BookVo bookVo, 
+										  @RequestParam("hb_no") int hb_no, 
+										  @RequestParam("file") MultipartFile file) {
+		System.out.println("[BookController] registerHopeBookConfirm()");
+		
+		System.out.println("hb_no: " + hb_no);
+		
+		String nextPage = "admin/book/register_book_ok";
+		
+		// SAVE FILE
+		String savedFileName = uploadFileService.upload(file);
+		
+		if (savedFileName != null) {
+			bookVo.setB_thumbnail(savedFileName);
+			int result = bookService.registerHopeBookConfirm(bookVo, hb_no);
+			
+			if (result <= 0)
+				nextPage = "admin/book/register_book_ng";
+		
+		} else {
+			nextPage = "admin/book/register_book_ng";
+		
+		}
+		
+		return nextPage;
+
+	}
+	
+	/*
+	 * 전체 도서 목록
+	 */
+//	@RequestMapping(value = "/getAllBooks", method = RequestMethod.GET)
+	@GetMapping("/getAllBooks")
+	public String getAllBooks(Model model) {
+		System.out.println("[BookController] getAllBooks()");
+		
+		String nextPage = "admin/book/full_list_of_books";
+		
+		List<BookVo> bookVos = bookService.getAllBooks();
+		
+		model.addAttribute("bookVos", bookVos);
 		
 		return nextPage;
 		
--- BookRentalPtj/src/main/java/com/office/library/book/admin/BookService.java
+++ BookRentalPtj/src/main/java/com/office/library/book/admin/BookService.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.office.library.book.BookVo;
+import com.office.library.book.HopeBookVo;
 import com.office.library.book.RentalBookVo;
 
 @Service
 //@Service("admin.BookService")
 public class BookService {
 
-	final static public int BOOK_ISBN_ALREADY_EXIST = 0;		// 이미 등록된 도서
+	final static public int BOOK_ISBN_ALREADY_EXIST = 0;	// 이미 등록된 도서
 	final static public int BOOK_REGISTER_SUCCESS = 1;		// 신규 도서 등록 성공
 	final static public int BOOK_REGISTER_FAIL = -1;		// 신규 도서 등록 실패
 	
@@ -91,6 +92,46 @@ public class BookService {
 			result = bookDao.updateBook(b_no);
 			
 		return result;
+	}
+	
+	public List<HopeBookVo> getHopeBooks() {
+		System.out.println("[BookService] getHopeBooks()");
+		
+		return bookDao.selectHopeBooks();
+		
+	}
+	
+	public int registerHopeBookConfirm(BookVo bookVo, int hb_no) {
+		System.out.println("[BookService] registerHopeBookConfirm()");
+		
+		boolean isISBN = bookDao.isISBN(bookVo.getB_isbn());
+		
+		if (!isISBN) {
+			int result = bookDao.insertBook(bookVo);
+			
+			if (result > 0) {
+				
+				bookDao.updateHopeBookResult(hb_no);
+				
+				return BOOK_REGISTER_SUCCESS;
+				
+			} else {
+				return BOOK_REGISTER_FAIL;
+				
+			}
+			
+		} else {
+			return BOOK_ISBN_ALREADY_EXIST;
+			
+		}
+		
+	}
+	
+	public List<BookVo> getAllBooks() {
+		System.out.println("[BookService] getAllBooks()");
+		
+		return bookDao.selectAllBooks();
+		
 	}
 	
 	
